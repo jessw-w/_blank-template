b40a9644bdd55085ea7641212ad2e87d27330744
jessw-w <147701451+jessw-w@users.noreply.github.com>
2023-11-30 17:18:13 +0800

    add

--- sketch.js
+++ sketch.js
@@ -2,20 +2,57 @@ var textfield;
 var fruits; // Declare fruits globally
 var correctWord; // Declare correctWord globally
 
+// Our object for the player/boss
+class Person {
+  constructor (name, hp, atk) {
+    this.name = name;
+    this.hp = hp;
+    this.atk = atk
+  }
+
+  takeDMG(dmg) {
+    this.hp -= dmg
+  }
+
+  getATK() {
+    return this.atk
+  }
+
+  getHP() {
+    return this.hp
+  }
+
+  getName() {
+    return this.name
+  }
+}
+
+// Set up our boss and player
+const boss = new Person("Boss", 100, 10);
+const player = new Person("Player", 100, 10);
+
 function setup() {
   input = createInput();
-  let canvas = createCanvas(500, 400);
+  let canvas = createCanvas(800, 600);
   canvas.parent("canvasContainer");
-  background(220);
 
-  // This variable stores whatever the user types
+  // This variable stores whatever the player types
   textfield = ""
+  redrawBackground();
 
+  // Load our fruit list and then get the first fruit
   loadFruits().then(() => {
     correctWord = getFruit();
-  console.log(correctWord);
-  console.log(fruits);
-});
+    // console.log(correctWord);
+    // console.log(fruits);
+  });
+
+}
+
+// Redraw the background
+function redrawBackground() {
+  clear();
+  background(220);
 }
 
 // Function to fetch and load the JSON file
@@ -36,42 +73,81 @@ function loadFruits() {
 }
 
 // Call the function to load fruits when needed
-
 function getFruit() {
   let index = floor(random(fruits.length));
-  return fruits[index];
+  return fruits[index].toLowerCase();
 }
 
-
 function draw() {
-  text(textfield, 100, 100);  // For debugging
+  //text(textfield, 100, 100);  // For debugging
   
   textSize(16)
   text(correctWord, 50,50);
+  reset = false
   
-  if (checkWord(correctWord)) {
-    text("Correct!", 100, 150)
-  } //else {
-    //text("Not quite", 100, 150)
-  //}
+  // Check if the player typed the wrong word
+  if (wrongLetter(correctWord)) {
+    text("ur fucked", 120, 120);
+    reset = true
+
+    player.takeDMG(boss.getATK())
+  }
+
+  // Check if the player finished typing
+  if (textfield == correctWord) {
+    text("Correct!", 100, 150);
+    reset = true
+
+    // Do dmg to the boss
+    boss.takeDMG(player.getATK());
+  }
+
+  if (reset) {
+    // Redraw the canvas
+    redrawBackground();
+
+    // Start to reset our words
+    textfield = "";  // reset our textfield
+    correctWord = getFruit();  // get our new word
+  }
+
+  // Check if won
+  if (boss.getHP() <= 0) {
+    // TODO: change this
+    redrawBackground();
+    text("You Won", 50, 50);
+  }
+
+  // Check if lost
+  if (player.getHP() <= 0) {
+    // TODO: change this
+    redrawBackground();
+    text("You Lose", 50, 50)
+  }
+
+  // Draw healthbar
+  // TODO: change this
+  text("Player HP: " + player.getHP(), 10, 10)
+  text("Boss HP: " + boss.getHP(), 10, 30)
+
+  // rect(50,50,65,5)
+  // Fill(red)
+  // rect(50,50,60,5)
 }
 
 
-// When the user type anything, it will be added to textfield
+// When the player type anything, it will be added to textfield
 function keyTyped() {
   textfield += key;
 }
 
-// This function should check if the user is in the process of typing the correctWord
-function checkWord(correctWord) { 
-  if(textfield.length === 0){
-    return false;
-  }
+// This function should check if the player is in the process of typing the correctWord
+function wrongLetter(correctWord) { 
   for (let i = 0; i < textfield.length; i++) {
     if (textfield[i] != correctWord[i]) {
-      return false
+      return true
     }
   }
 
-  return true
+  return false
 }
